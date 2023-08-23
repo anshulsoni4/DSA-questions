3472283fff16aa5ca4475ae54696803ba3327038
Anshul Soni <74638335+anshulsoni4@users.noreply.github.com>
2023-08-23 11:18:19 +0530

    Add files via upload

---
+++ sum_of_arrays.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   vector<int> arr = {1,6,3,7,3};
+   int n = arr.size();
+   
+   int sum = 0;
+   for(int i=0; i<n; i++){
+       
+    
+       sum += arr[i];
+   }
+   
+   cout<<sum;
+   
+   
+}
