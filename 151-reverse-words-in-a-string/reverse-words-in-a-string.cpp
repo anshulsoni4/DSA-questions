6cbb822eed5a2660b00a31ccb589ab1be8b8d7a2
Anshul Soni <74638335+anshulsoni4@users.noreply.github.com>
2023-10-27 11:31:07 +0530

    Time: 4 ms (60.74%) | Memory: 21.2 MB (20.34%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n =s.size(), i=0;
+
+        // make a empty string
         string ans = "";
         
         while(i<n){
+            // ek temporary string bnayi
             string temp = "";
             
+            // iterate kiya, and first agar khali space hai, toh aage chalo
             while(s[i] == ' ' && i<n){
                 i++;
             }
+            // ab space nahi hai, element hai, toh, temp me daaldo, aur chalo
             while(s[i] != ' ' && i<n){
                 temp += s[i];
                 i++;
